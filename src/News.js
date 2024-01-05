66404a7382d30350db01c0fa961494c3dede9a35
Omkarmorajkar <omkarmorajkar22@gmail.com>
2024-01-05 16:39:34 +0530

    ..

--- src/News.js
+++ src/News.js
@@ -6,20 +6,24 @@ export default function News(props) {
   return (
     <div className="News">
       <div className="news-img">
-        {
-          article.urlToImage !== null ? (
-            <img src={article.urlToImage} alt={article.title} />
-          ) : (
-            <img src="https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg" alt="Default Image" />
-          )
-        }
-      </div> 
+        {article.urlToImage !== null ? (
+          <img src={article.urlToImage} alt={article.title} />
+        ) : (
+          <img
+            src="https://t4.ftcdn.net/jpg/02/51/95/53/360_F_251955356_FAQH0U1y1TZw3ZcdPGybwUkH90a3VAhb.jpg"
+            alt="Default"
+          />
+        )}
+      </div>
       <h1>{article.title}</h1>
       <p>
         {article.description?.substring(0, 100).concat("...")}
-        <a href={article.url} target={"_blank"} rel="noopener noreferrer"> Read More</a>
+        <a href={article.url} target={"_blank"} rel="noopener noreferrer">
+          {" "}
+          Read More
+        </a>
       </p>
-      
+
       <div className="source">
         <p> Author: {article.author}</p>
         <p>{article.source.name}</p>
